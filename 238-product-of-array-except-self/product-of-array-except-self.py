fb7aea94d1bb406d877ef0507746575bfb18680f
kiranmai puli <32175859+kiranmaipuli@users.noreply.github.com>
2025-05-17 14:41:59 -0700

    Time: 27 ms (47.79%) | Memory: 23.1 MB (93.63%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,27 +1,64 @@
+# class Solution:   Space complexity: O(N), Time complexity: O(N)
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         prefixMultArrFw = []
+#         prefixMultArrBw = [0] * len(nums)
+#         for index in range(len(nums)):
+#             if index == 0:
+#                 prefixMultArrFw.append(nums[index])
+#                 prefixMultArrBw[len(nums)-1] = nums[len(nums)-1]
+#             else:
+#                 prefixMultArrFw.append(prefixMultArrFw[index-1]*nums[index])
+
+#         for index in range(len(nums)-1, -1, -1):
+#             if index == (len(nums)-1):                
+#                 prefixMultArrBw[index] = nums[index]
+#             else:
+#                 prefixMultArrBw[index] = prefixMultArrBw[index+1]*nums[index]
+
+#         for index in range(len(nums)):
+
+#                 if index == 0:
+#                     nums[index] = prefixMultArrBw[index+1]
+#                 elif index + 1 < len(nums):
+#                     nums[index] = prefixMultArrFw[index-1] * prefixMultArrBw[index+1]
+#                 else:
+#                     nums[index] = prefixMultArrFw[index-1]
+
+#         return nums
+
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prefixMultArrFw = []
-        prefixMultArrBw = [0] * len(nums)
+        prefixMultArrFw = [1]
+        counter = 1
+        rightSuffix = 1
         for index in range(len(nums)):
-            if index == 0:
-                prefixMultArrFw.append(nums[index])
-                prefixMultArrBw[len(nums)-1] = nums[len(nums)-1]
+            if counter < len(nums):
+                # print(prefixMultArrFw[index], nums[index], "prefixMultArrFw[index], nums[index]")
+                prefixMultArrFw.append(prefixMultArrFw[index] * nums[index])
+                counter += 1
             else:
-                prefixMultArrFw.append(prefixMultArrFw[index-1]*nums[index])
-
+                break
+        # print(prefixMultArrFw)        
         for index in range(len(nums)-1, -1, -1):
-            if index == (len(nums)-1):                
-                prefixMultArrBw[index] = nums[index]
-            else:
-                prefixMultArrBw[index] = prefixMultArrBw[index+1]*nums[index]
+            prefixMultArrFw[index] = prefixMultArrFw[index] * rightSuffix
+            if index-1 >= 0:
+                rightSuffix *= nums[index]
+                # print(rightSuffix, "cbdbchdbc")
+        # print(prefixMultArrFw)
+        return prefixMultArrFw
 
-        for index in range(len(nums)):
 
-                if index == 0:
-                    nums[index] = prefixMultArrBw[index+1]
-                elif index + 1 < len(nums):
-                    nums[index] = prefixMultArrFw[index-1] * prefixMultArrBw[index+1]
-                else:
-                    nums[index] = prefixMultArrFw[index-1]
 
-        return nums
+
+
+
+
+
+
+
+
+
+
+
+
