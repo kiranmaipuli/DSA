d1ac0d909a6e23413533550e8c88b3784dcfaf36
kiranmai puli <32175859+kiranmaipuli@users.noreply.github.com>
2025-05-17 11:32:17 -0700

    Time: 61 ms (5.16%) | Memory: 26.9 MB (21.55%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -0,0 +1,28 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prefixMultArrFw = []
+        prefixMultArrBw = [0] * len(nums)
+        for index in range(len(nums)):
+            if index == 0:
+                prefixMultArrFw.append(nums[index])
+                prefixMultArrBw[len(nums)-1] = nums[len(nums)-1]
+            else:
+                prefixMultArrFw.append(prefixMultArrFw[index-1]*nums[index])
+
+        for index in range(len(nums)-1, -1, -1):
+            if index == (len(nums)-1):                
+                prefixMultArrBw[index] = nums[index]
+            else:
+                prefixMultArrBw[index] = prefixMultArrBw[index+1]*nums[index]
+
+        print(prefixMultArrFw, prefixMultArrBw)
+        for index in range(len(nums)):
+
+                if index == 0:
+                    nums[index] = prefixMultArrBw[index+1]
+                elif index + 1 < len(nums):
+                    nums[index] = prefixMultArrFw[index-1] * prefixMultArrBw[index+1]
+                else:
+                    nums[index] = prefixMultArrFw[index-1]
+
+        return nums
