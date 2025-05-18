6cd926f77a08f44525a394fc991dc0ed03ecf96e
kiranmai puli <32175859+kiranmaipuli@users.noreply.github.com>
2025-05-17 21:00:06 -0700

    Time: 12 ms (60.33%) | Memory: 21.1 MB (50.30%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -9,3 +9,17 @@ class Solution:
                 anagram_dict[sorted_string] = len(output)-1
             output[anagram_dict[sorted_string]].append(word)    
         return output
+
+# class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         anagram_dict = {}
+#         output = []        
+#         for word in strs:
+#             sorted_string = ''.join(sorted(word))
+#             if sorted_string not in anagram_dict:
+#                 anagram_dict[sorted_string] = [word]
+#             else:
+#                 anagram_dict[sorted_string].append(word)
+#         for key in anagram_dict:
+#             output.append(anagram_dict[key])
+#         return output
