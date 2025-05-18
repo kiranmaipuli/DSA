26b27c96c67fc2e513473f09795445322df7a6cd
kiranmai puli <32175859+kiranmaipuli@users.noreply.github.com>
2025-05-17 20:59:15 -0700

    Time: 8 ms (92.88%) | Memory: 21 MB (50.30%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -7,9 +7,5 @@ class Solution:
             if sorted_string not in anagram_dict:
                 output.append([])    
                 anagram_dict[sorted_string] = len(output)-1
-            print(anagram_dict[sorted_string])
             output[anagram_dict[sorted_string]].append(word)    
-                # anagram_dict[sorted_string].append(word)
-        # for key in anagram_dict:
-        #     output.append(anagram_dict[key])
         return output
