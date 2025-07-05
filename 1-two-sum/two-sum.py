095ad1a04ebffbf8247ed342e338a3fd0915de60
kiranmai puli <32175859+kiranmaipuli@users.noreply.github.com>
2025-07-04 18:16:57 -0700

    Time: 0 ms (100.00%) | Memory: 18.7 MB (62.27%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -2,13 +2,29 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
         # Brute force
+        # output = []
+        # for i in range(0,len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if (nums[i] + nums[j]) == target:
+        #             output.append(i)
+        #             output.append(j)
+        #             break
+        #     if len(output) > 0:
+        #         break
+        # return output
+
+
+        # Hashmap solution
         output = []
-        for i in range(0,len(nums)):
-            for j in range(i+1, len(nums)):
-                if (nums[i] + nums[j]) == target:
-                    output.append(i)
-                    output.append(j)
-                    break
-            if len(output) > 0:
+        hasmap_dict = {}
+        for index in range(len(nums)):
+            hasmap_dict[nums[index]] = index
+        
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in hasmap_dict and i != hasmap_dict[diff]:
+                output.append(i)
+                output.append(hasmap_dict[diff])
                 break
+        
         return output
