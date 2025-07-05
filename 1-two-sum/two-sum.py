687a0ffe238ba56e766eff60487ca0d3cc1f853c
kiranmai puli <32175859+kiranmaipuli@users.noreply.github.com>
2025-07-04 17:54:03 -0700

    Time: 1751 ms (25.30%) | Memory: 18.3 MB (87.15%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,15 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        numsDict = {}
-        for index,value in enumerate(nums):
-            if value not in numsDict:
-                numsDict[value] = [index]
-            else:
-                numsDict[value].append(index)
-                
-        for index,value in enumerate(nums):
-            print(target - value)
-            if (target - value) in numsDict:
-                for i in numsDict[target - value]:
-                    if i != index:
-                        return [index,i]
+
+        # Brute force
+        output = []
+        for i in range(0,len(nums)):
+            for j in range(i+1, len(nums)):
+                if (nums[i] + nums[j]) == target:
+                    output.append(i)
+                    output.append(j)
+                    break
+            if len(output) > 0:
+                break
+        return output
